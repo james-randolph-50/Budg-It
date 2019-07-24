cd2dbfa63429394c9b1fb99d250b7edfbeca5bbf
James Randolph <james.randolph2016@gmail.com>
2019-07-24 17:06:30 -0400

    UI, Budget, App modules set

---
+++ app.js
@@ -0,0 +1,18 @@
+// BUDGET CONTROLLER/MODULE
+
+const budgetController = (function() {
+
+    
+})();
+
+// UI CONTROLLER/MODULE
+
+const UIController = (function() {
+
+})();
+
+// APP CONTROLLER
+
+const controller = (function(budgetCtrl, UICtrl) {
+
+})(budgetController, UIController);
