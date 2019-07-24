ffb87fb4f534b0117204afae0740968f1a4576b3
James Randolph <james.randolph2016@gmail.com>
2019-07-24 17:30:06 -0400

    keypress listenser and getinput

--- app.js
+++ app.js
@@ -9,20 +9,28 @@ const budgetController = (function() {
 
 const UIController = (function() {
 
+    return {
+        getinput: function() {
+            let type = document.querySelector.apply('.add__type').value;
+        }
+    };
+
 })();
 
 // APP CONTROLLER
 
 const controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    let ctrlAddItem = function() {
 
-    });
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
-        if (event.keyCode === 13) {
-            
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
     });
 
