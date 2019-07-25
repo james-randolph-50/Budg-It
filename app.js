fa42f461f1824f9cf28c907777d33ab0bdf21e9c
James Randolph <james.randolph2016@gmail.com>
2019-07-25 07:35:43 -0400

    adding html with dynamic values

--- app.js
+++ app.js
@@ -73,6 +73,28 @@ const UIController = (function() {
             };
         },
 
+        addListItem: function(obj, type) {
+            let html, newHtml;
+
+            // Create HTML string with placeholder text
+
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            // Replace placeholder text with some data
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert HTML into the DOM
+            
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
