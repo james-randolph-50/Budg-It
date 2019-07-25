43dbbc0cb1cdf4f2159a4adf8f14d4c4e35b3747
James Randolph <james.randolph2016@gmail.com>
2019-07-25 13:08:59 -0400

    format numbers

--- app.js
+++ app.js
@@ -235,6 +235,23 @@ const UIController = (function() {
             });
         },
 
+        formatNumber: function(num, type) {
+            let numSplit, inc, dec;
+
+            num = Math.abs(num);
+            num = num.toFixed(2);
+
+            numSplit = num.split('.');
+
+            int = numSplit[0];
+            if (int.length > 3) {
+                int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+            }
+
+            dec = numSplit[1];
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
